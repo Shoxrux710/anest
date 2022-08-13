f0bad85b5f4eba0bfb23d0ddfcb7c13f43931ecf
Shoxrux710 <shoxruxbuxorov710@gmail.com>
2022-08-13 12:38:27 +0500

    Sdfsdfsd

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "anest",
+            script: "server.js",
+            env: {
+                "NODE_ENV": "default"
+            }
+        }
+    ]
+}
